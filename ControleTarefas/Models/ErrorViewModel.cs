2dd71fed4bd3c5257f1c43dfe756f03382a2cdbd
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2020-06-06 10:51:13 -0300

    Commit Inicial

---
+++ ControleTarefas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ControleTarefas.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
